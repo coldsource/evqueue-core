6e4ed1c9949fdddd64459345158bebc7adb4c2b6
Thibault KUMMER <bob@coldsource.net>
2015-04-15 11:03:33 +0200

    Sequence generator, which will be used on savepoint levels 0 and 1

---
+++ SequenceGenerator.cpp
@@ -0,0 +1,52 @@
+/*
+ * This file is part of evQueue
+ * 
+ * evQueue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * evQueue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with evQueue. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Thibault Kummer <bob@coldsource.net>
+ */
+
+#include <SequenceGenerator.h>
+#include <Configuration.h>
+#include <DB.h>
+
+SequenceGenerator *SequenceGenerator::instance = 0;
+
+SequenceGenerator::SequenceGenerator()
+{
+	instance = this;
+	
+	pthread_mutex_init(&lock, NULL);
+	
+	DB db;
+	db.Query("SELECT MAX(workflow_instance_id) FROM t_workflow_instance");
+	db.FetchRow();
+	seq = db.GetFieldInt(0)+1;
+}
+
+SequenceGenerator::~SequenceGenerator()
+{
+	;
+}
+
+unsigned int SequenceGenerator::GetInc()
+{
+	unsigned int cur_seq;
+	
+	pthread_mutex_lock(&lock);
+	cur_seq = seq++;
+	pthread_mutex_unlock(&lock);
+	
+	return cur_seq;
+}
---
+++ include/SequenceGenerator.h
@@ -0,0 +1,42 @@
+/*
+ * This file is part of evQueue
+ * 
+ * evQueue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * evQueue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with evQueue. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Thibault Kummer <bob@coldsource.net>
+ */
+
+#ifndef _SEQUENCEGENERATOR_H_
+#define _SEQUENCEGENERATOR_H_
+
+#include <pthread.h>
+
+class SequenceGenerator
+{
+	private:
+		pthread_mutex_t lock;
+		unsigned int seq;
+		
+		static SequenceGenerator* instance;
+		
+	public:
+		SequenceGenerator();
+		~SequenceGenerator();
+		
+		static SequenceGenerator *GetInstance() { return instance; }
+		
+		unsigned int GetInc();
+};
+
+#endif
