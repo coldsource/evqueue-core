03ca797f8e2dae0e1ed634b460bd76b2a57acfe9
Bob <bob@coldsource.net>
2015-06-11 20:20:15 +0200

    Display build date

--- evqueue.cpp
+++ evqueue.cpp
@@ -127,7 +127,7 @@ int main(int argc,const char **argv)
 			return tools_queue_stats();
 		else if(strcmp(argv[i],"--version")==0)
 		{
-			printf("evQueue version 1.4\n");
+			printf("evQueue version 1.4 (built " __DATE__ ")\n");
 			return 0;
 		}
 		else
