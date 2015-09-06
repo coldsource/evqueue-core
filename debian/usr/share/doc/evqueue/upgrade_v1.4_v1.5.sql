10d1ecad24ad6351cf4592fbbc122d2caec2f548
Thibault Kummer <bob@coldsource.net>
2015-09-06 23:37:42 +0200

    Preparing version 1.5

---
+++ debian/usr/share/doc/evqueue/upgrade_v1.4_v1.5.sql
@@ -0,0 +1,17 @@
+ALTER TABLE t_workflow_instance ADD COLUMN node_name VARCHAR(32) COLLATE 'ascii_general_ci' NOT NULL DEFAULT '' AFTER workflow_instance_id;
+
+-- Alter version number --
+ALTER TABLE t_log COMMENT 'v1.5';
+ALTER TABLE t_notification COMMENT 'v1.5';
+ALTER TABLE t_notification_type COMMENT 'v1.5';
+ALTER TABLE t_queue COMMENT 'v1.5';
+ALTER TABLE t_schedule COMMENT 'v1.5';
+ALTER TABLE t_task COMMENT 'v1.5';
+ALTER TABLE t_user COMMENT 'v1.5';
+ALTER TABLE t_user_right COMMENT 'v1.5';
+ALTER TABLE t_workflow COMMENT 'v1.5';
+ALTER TABLE t_workflow_instance COMMENT 'v1.5';
+ALTER TABLE t_workflow_instance_parameters COMMENT 'v1.5';
+ALTER TABLE t_workflow_notification COMMENT 'v1.5';
+ALTER TABLE t_workflow_schedule COMMENT 'v1.5';
+ALTER TABLE t_workflow_schedule_parameters COMMENT 'v1.5';
