3dbfeb2cd283a553f3cf48f70fb177ce3b46b64d
Thibault Kummer <bob@coldsource.net>
2016-08-01 22:32:37 +0200

    XSD to .cpp converter

---
+++ convert_xsd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in `ls *.xsd`
+do
+	CONST_NAME=${i//'.'/'_'};
+	cat license.header >$i.cpp
+	echo "#include <string>" >>$i.cpp
+	echo >>$i.cpp
+	echo -n "std::string ${CONST_NAME}_str = \"" >>$i.cpp;
+	cat workflow.xsd | sed 's/$/ \\/g' | sed 's/"/\\"/g' >>$i.cpp
+	echo >>$i.cpp
+	echo -n '";' >>$i.cpp
+	echo >>$i.cpp
+done
