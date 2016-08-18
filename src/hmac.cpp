32238a0f7cd165aadfe1138439147e2911dc84ed
Thibault KUMMER <tkummer@quechoisir.org>
2016-08-18 12:42:34 +0200

    Work on binary data

--- src/hmac.cpp
+++ src/hmac.cpp
@@ -19,23 +19,55 @@
 
 #include <hmac.h>
 #include <sha1.h>
+#include <Exception.h>
 
 #include <iomanip>
 #include <sstream>
 
 using namespace std;
 
+static int hexchar2bin(char c)
+{
+	if(c>='0' && c<='9')
+		return c-'0';
+	else if(c>='a' && c<='f')
+		return c-'a';
+	else if(c>='A' && c<='F')
+		return c-'A';
+	else
+		throw Exception("hmac","Invalid hexadecimal string");
+}
+
+static string hex2bin(const string &hex)
+{
+	if(hex.length()%2!=0)
+		throw Exception("hmac","Invalid hexadecimal string");
+	
+	string bin;
+	for(int i=0;i<hex.length();i+=2)
+	{
+		char c = ((hexchar2bin(hex.at(i))&0x0F)<<4) | (hexchar2bin(hex.at(i+1))&0x0F);
+		bin.append(1,c);
+	}
+	
+	return bin;
+}
+
 string hash_hmac(const string &key, const string &data)
 {
+	// Decode input hexadecimal strings to binary
+	string bkey = hex2bin(key);
+	string bdata = hex2bin(data);
+	
 	string ikey, okey;
 	
 	//  Prepare inner and outer key by adding padding
 	for(int i=0;i<64;i++)
 	{
-		if(i<key.length())
+		if(i<bkey.length())
 		{
-			ikey.append(1,key[i]^0x36);
-			okey.append(1,key[i]^0x5c);
+			ikey.append(1,bkey[i]^0x36);
+			okey.append(1,bkey[i]^0x5c);
 		}
 		else
 		{
@@ -50,7 +82,7 @@ string hash_hmac(const string &key, const string &data)
 	
 	sha1_init_ctx(&ctx);
 	sha1_process_bytes(ikey.c_str(),ikey.length(),&ctx);
-	sha1_process_bytes(data.c_str(),data.length(),&ctx);
+	sha1_process_bytes(bdata.c_str(),bdata.length(),&ctx);
 	sha1_finish_ctx(&ctx,c_hash);
 	
 	// Compute outer hash, that is HMAC
