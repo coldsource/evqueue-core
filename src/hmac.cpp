32b4f9e8af94749d4ab67cea5c0c7771253f216d
Thibault KUMMER <tkummer@quechoisir.org>
2016-08-18 15:17:18 +0200

    Error converting hex to dec

--- src/hmac.cpp
+++ src/hmac.cpp
@@ -31,9 +31,9 @@ static int hexchar2bin(char c)
 	if(c>='0' && c<='9')
 		return c-'0';
 	else if(c>='a' && c<='f')
-		return c-'a';
+		return c-'a'+10;
 	else if(c>='A' && c<='F')
-		return c-'A';
+		return c-'A'+10;
 	else
 		throw Exception("hmac","Invalid hexadecimal string");
 }
