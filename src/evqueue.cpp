6dafae3e29f218cbc6cba436d7aa4a2c04e05b7e
Thibault KUMMER <tkummer@quechoisir.org>
2017-08-25 09:30:59 +0200

    Removed debug code

--- src/evqueue.cpp
+++ src/evqueue.cpp
@@ -144,56 +144,6 @@ void fork_child_handler(void)
 }
 
 using namespace std;
-void dump_expr(TokenExpr *expr, int level);
-void dump_token(Token *token,int level);
-
-void dump_token(Token *token,int level)
-{
-	printf("%d %s\n",level,Token::ToString(token->GetType()).c_str());
-	
-	if(token->GetType()==FUNC)
-	{
-		TokenFunc *func = (TokenFunc *)token;
-		for(int j=0;j<func->args.size();j++)
-		{
-			printf("arg %d :\n",j);
-			dump_expr(func->args.at(j),0);
-		}
-		printf("end args\n");
-	}
-	else if(token->GetType()==NODENAME)
-		TokenNodeName *node = (TokenNodeName *)token;
-	else if(token->GetType()==SEQ)
-	{
-		TokenSeq *seq = (TokenSeq *)token;
-		
-		printf("Sequence %ld items\n",seq->items.size());
-	}
-	else if(token->GetType()==FILTER)
-	{
-		dump_expr(((TokenFilter *)token)->filter,0);
-	}
-	else if(token->GetType()==LIT_INT)
-		printf("val = %d\n",((TokenInt *)token)->i);
-	else if(token->GetType()==LIT_FLOAT)
-		printf("val = %f\n",((TokenFloat *)token)->d);
-	else if(token->GetType()==LIT_BOOL)
-		printf("val = %s\n",((TokenBool *)token)->b?"true":"false");
-	else if(token->GetType()==LIT_STR)
-		printf("val = %s\n",((TokenString *)token)->s.c_str());
-}
-
-void dump_expr(TokenExpr *expr, int level)
-{
-	for(int i=0;i<expr->expr_tokens.size();i++)
-	{
-		dump_token(expr->expr_tokens.at(i),level);
-		
-		if(expr->expr_tokens.at(i)->GetType()==EXPR)
-			dump_expr((TokenExpr *)expr->expr_tokens.at(i),level+1);
-	}
-}
-
 
 int main(int argc,const char **argv)
 {
