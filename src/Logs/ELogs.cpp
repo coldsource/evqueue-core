a6c167b7080c903788ee886c030cfb870b762201
Thibault KUMMER <thibault.kummer@yellowkorner.fr>
2022-03-21 11:48:54 +0100

    Added all logs filters

--- src/Logs/ELogs.cpp
+++ src/Logs/ELogs.cpp
@@ -49,6 +49,27 @@ bool ELogs::HandleQuery(const User &user, XMLQuery *query, QueryResponse *respon
 		string filter_ip = query->GetRootAttribute("filter_ip","");
 		string filter_ip_bin;
 		
+		string filter_channel = query->GetRootAttribute("filter_channel","");
+		
+		string filter_uid = query->GetRootAttribute("filter_uid","");
+		
+		string filter_status_str = query->GetRootAttribute("filter_status","");
+		int filter_status;
+		if(filter_status_str!="")
+		{
+			try
+			{
+				filter_status = stoi(filter_status_str);
+			}
+			catch(...)
+			{
+				throw Exception("ELogs","Attribute filter_status must be an integer","INVALID_PARAMETER");
+			}
+		}
+		
+		string filter_domain = query->GetRootAttribute("filter_domain","");
+		string filter_machine = query->GetRootAttribute("filter_machine","");
+		
 		DB db;
 		
 		string query_select;
@@ -60,17 +81,19 @@ bool ELogs::HandleQuery(const User &user, XMLQuery *query, QueryResponse *respon
 		
 		query_select =" \
 			SELECT \
-				elog_date, \
-				elog_crit, \
+				ec.elog_channel_name, \
+				e.elog_date, \
+				e.elog_crit, \
 				epmachine.elog_pack_string AS elog_machine, \
 				epdomain.elog_pack_string AS elog_domain, \
-				elog_ip, \
-				elog_uid, \
-				elog_status, \
-				elog_fields ";
+				e.elog_ip, \
+				e.elog_uid, \
+				e.elog_status, \
+				e.elog_fields ";
 	
 		query_from = " \
 			FROM t_elog e \
+			INNER JOIN t_elog_channel ec ON e.elog_channel_id=ec.elog_channel_id \
 			LEFT JOIN t_elog_pack epmachine ON e.elog_machine=epmachine.elog_pack_id \
 			LEFT JOIN t_elog_pack epdomain ON e.elog_domain=epdomain.elog_pack_id ";
 		
@@ -79,27 +102,57 @@ bool ELogs::HandleQuery(const User &user, XMLQuery *query, QueryResponse *respon
 		if(filter_crit_str!="")
 		{
 			filter_crit = ls->PackCrit(filter_crit_str);
-			query_where += " AND elog_crit = %i ";
+			query_where += " AND e.elog_crit = %i ";
 			values.push_back(&filter_crit);
 		}
 		
 		if(filter_emitted_from!="")
 		{
-			query_where += " AND elog_date>=%s ";
+			query_where += " AND e.elog_date>=%s ";
 			values.push_back(&filter_emitted_from);
 		}
 		
 		if(filter_emitted_until!="")
 		{
-			query_where += " AND elog_date<=%s ";
+			query_where += " AND e.elog_date<=%s ";
 			values.push_back(&filter_emitted_until);
 		}
 		
 		if(filter_ip!="")
 		{
 			filter_ip_bin = ls->PackIP(filter_ip);
-			query_where += " AND elog_ip=%s ";
+			query_where += " AND e.elog_ip=%s ";
 			values.push_back(&filter_ip_bin);
+		}
+		
+		if(filter_channel!="")
+		{
+			query_where += " AND ec.elog_channel_name=%s ";
+			values.push_back(&filter_channel);
+		}
+		
+		if(filter_uid!="")
+		{
+			query_where += " AND e.elog_uid=%s ";
+			values.push_back(&filter_uid);
+		}
+		
+		if(filter_status_str!="")
+		{
+			query_where += " AND e.elog_status=%i ";
+			values.push_back(&filter_status);
+		}
+		
+		if(filter_domain!="")
+		{
+			query_where += " AND epdomain.elog_pack_string=%s ";
+			values.push_back(&filter_domain);
+		}
+		
+		if(filter_machine!="")
+		{
+			query_where += " AND epmachine.elog_pack_string=%s ";
+			values.push_back(&filter_machine);
 		}
 		
 		query_order = " ORDER BY elog_date DESC,elog_id DESC ";
@@ -113,14 +166,16 @@ bool ELogs::HandleQuery(const User &user, XMLQuery *query, QueryResponse *respon
 		while(db.FetchRow())
 		{
 			DOMElement node = (DOMElement)response->AppendXML("<log />");
-			node.setAttribute("date",db.GetField(0));
-			node.setAttribute("crit",ls->UnpackCrit(db.GetFieldInt(1)));
-			node.setAttribute("machine",db.GetField(2));
-			node.setAttribute("domain",db.GetField(3));
-			node.setAttribute("ip",ls->UnpackIP(db.GetField(4)));
-			node.setAttribute("uid",db.GetField(5));
-			node.setAttribute("status",db.GetField(6));
-			node.setAttribute("custom_fields",db.GetField(7));
+			node.setAttribute("channel",db.GetField(0));
+			node.setAttribute("date",db.GetField(1));
+			node.setAttribute("crit",ls->UnpackCrit(db.GetFieldInt(2)));
+			node.setAttribute("machine",db.GetField(3));
+			node.setAttribute("domain",db.GetField(4));
+			if(db.GetField(5)!="")
+				node.setAttribute("ip",ls->UnpackIP(db.GetField(5)));
+			node.setAttribute("uid",db.GetField(6));
+			node.setAttribute("status",db.GetField(7));
+			node.setAttribute("custom_fields",db.GetField(8));
 		}
 		
 		return true;
